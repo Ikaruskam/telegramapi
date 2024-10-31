6014da79c10f4b733076b9412a4e38e773b34c17
Ikaruskam <45374569+Ikaruskam@users.noreply.github.com>
2024-10-31 10:43:10 +0300

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -22,7 +22,7 @@ function App() {
     <>
       {items && items.map(item => {
         return <span style={{padding:'0px 4px'}} key={item.name} className="roll-out">
-          <img src={item.img} alt='logo' width="16" style={{padding:'0px 5px'}}></img>
+          <img src={item.img} alt='logo' width="40" style={{padding:'0px 5px'}}></img>
           <span>{item.name}</span>
         </span>
       })}
