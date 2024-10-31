fbe40165f812a1662e06c0d912313a95e780516d
Ikaruskam <45374569+Ikaruskam@users.noreply.github.com>
2024-10-31 10:13:27 +0300

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,7 +6,7 @@ function App() {
   const [items, setItems] = useState(null)
 
   const fetchItems = () => {
-    axios.get('http://194.247.187.229/items').then(r => {
+    axios.get('http://194.247.187.229/api/items').then(r => {
       setItems(r.data)
     })
   }
